731ea274965984738cb63c02b6f98e86725f4d0f
Ana Jasso <ana.jassov@gmail.com>
2024-10-22 15:46:39 -0600

    Implementación Google Maps

--- petveterinary-web/front-end/src/app/components/vistas/nosotros/nosotros.component.ts
+++ petveterinary-web/front-end/src/app/components/vistas/nosotros/nosotros.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { InactivityService } from 'src/app/services/inactivity-service.service';
 
 @Component({
@@ -6,7 +6,7 @@ import { InactivityService } from 'src/app/services/inactivity-service.service';
   templateUrl: './nosotros.component.html',
   styleUrls: ['./nosotros.component.css']
 })
-export class NosotrosComponent {
+export class NosotrosComponent implements OnInit{
 
   loader = true;
 
@@ -17,6 +17,31 @@ export class NosotrosComponent {
       this.loader = false;
     }, 2000);
     this.inactivityService.startTimer();
+  }
+
+  display: any;
+  center: google.maps.LatLngLiteral = {
+    lat: 21.158003,
+    lng: -100.940717
+  };
+  zoom = 6;
+
+  markerPositions: google.maps.LatLngLiteral[] = [
+    { lat: 21.158003, lng: -100.940717 } // Ubicación específica
+  ];
+
+  moveMap(event: google.maps.MapMouseEvent){
+    if(event.latLng != null) this.center = (event.latLng.toJSON());
+  }
+
+  move(event: google.maps.MapMouseEvent){
+    if(event.latLng != null) this.display = (event.latLng.toJSON());
+  }
+
+  // Método para acercar el mapa al marcador
+  zoomToMarker(markerPosition: google.maps.LatLngLiteral) {
+    this.center = markerPosition;
+    this.zoom = 30; 
   }
 
 }
